4b5263ed0e8259111d73b85566bfa858fff085de
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-12-27 19:30:22 +0600

    Update 1464. Maximum Product of Two Elements in an Array.cpp

--- 1464. Maximum Product of Two Elements in an Array.cpp
+++ 1464. Maximum Product of Two Elements in an Array.cpp
@@ -13,3 +13,24 @@ public:
         return mx;
     }
 };
+
+
+
+//or another solution
+
+// int max = 0;
+//       int smax = 0;
+
+//       for(int i = 0; i < nums.length; i++){
+
+//         if(max < nums[i]){
+//             smax = max;
+//             max = nums[i];
+//         }
+//         else if(smax < nums[i]){
+//             smax = nums[i];
+//         }
+//       }  
+//       int ans = (max - 1) * (smax - 1);
+
+//       return ans;
